af5f670ff0de24ee5fa92d773008b28d843a296f
unknown <suwandiaminsangaji@gmail.com>
2024-03-19 04:30:39 +0900

    login

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,15 +1,15 @@
 <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <a href="index.html">Stisla</a>
+      <a href="index.html">Deo Airport</a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
-      <a href="index.html">St</a>
+      <a href="index.html">DA</a>
     </div>
 
     <ul class="sidebar-menu">
         @section('sidebar')
         <li class="menu-header">Dashboard</li>
-        <li class="active"><a class="nav-link" href="blank.html"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+        <li class="active"><a class="nav-link" href="{{ route('home') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
         @show
       </ul>
   </aside>

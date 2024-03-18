b3a341935ae638f7733d2206d935c59d2ddb970a
unknown <suwandiaminsangaji@gmail.com>
2024-03-18 23:33:52 +0900

    templating

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="footer-left">
+    Copyright &copy; <?= date('Y') ?> <div class="bullet"></div> By <a href="https://portal.deoairport.co.id" target="_blank">Bandara Deo sorong</a>
+  </div>
+  <div class="footer-right">
+    2.3.0
+  </div>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,15 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.html">Stisla</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.html">St</a>
+    </div>
+
+    <ul class="sidebar-menu">
+        @section('sidebar')
+        <li class="menu-header">Dashboard</li>
+        <li class="active"><a class="nav-link" href="blank.html"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+        @show
+      </ul>
+  </aside>
